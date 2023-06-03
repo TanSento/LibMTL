9b83a95dc51d46ddf90e70fb779f804237c3eb19
TanSento <buitantendou96@gmail.com>
2023-06-03 13:50:00 +0800

    add load to numpy import

--- LibMTL/trainer.py
+++ LibMTL/trainer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import save
+from numpy import save, load
 
 
 from LibMTL._record import _PerformanceMeter
